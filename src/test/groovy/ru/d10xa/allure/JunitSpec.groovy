f743b55700ee5d6fad42308011d077c4e6fe6b9b
d10xa <d10xa@mail.ru>
2016-07-05 16:20:03 +0300

    Test junit with multiple versions of gradle

--- src/test/groovy/ru/d10xa/allure/JunitSpec.groovy
+++ src/test/groovy/ru/d10xa/allure/JunitSpec.groovy
@@ -1,48 +1,46 @@
 package ru.d10xa.allure
 
-import groovy.transform.Memoized
 import org.gradle.testkit.runner.GradleRunner
 import ru.d10xa.allure.extension.GradlePluginClasspath
 import ru.d10xa.allure.extension.TestProjectDir
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
+@Unroll
 class JunitSpec extends Specification {
 
-    @TestProjectDir(dir = "junit")
-    @Shared
+    @TestProjectDir(dir = "junit", clean = true)
     File testProjectDirectory
 
     @GradlePluginClasspath
     @Shared
     List<File> pluginClasspath
 
-    @Memoized
-    def getBuildResult() {
-        GradleRunner.create()
+    @Unroll
+    def 'html attachment found in report data with gradle verison #gradleVersion'() {
+        when:
+        def buildResult = GradleRunner.create()
                 .withProjectDir(testProjectDirectory)
                 .withTestKitDir(new File(testProjectDirectory.parentFile.absolutePath, '.gradle'))
                 .withArguments('test', 'allureReport')
+                .withGradleVersion(gradleVersion)
                 .withPluginClasspath(pluginClasspath)
                 .build()
-    }
 
-    def 'task successfully executed'() {
-        expect:
-        buildResult.task(":test").outcome == SUCCESS
-    }
-
-    def 'html attachment found in report data'() {
-        when:
         int attachmentsCount = new File(testProjectDirectory, 'build/allure-report/data')
                 .list()
                 .findAll { it.endsWith '-attachment.html' }
                 .size()
 
         then:
+        buildResult.task(":test").outcome == SUCCESS
         attachmentsCount == 1
+
+        where:
+        gradleVersion << ['2.8', '2.9', '2.10', '2.12', '2.13', '2.14']
     }
 
 }
